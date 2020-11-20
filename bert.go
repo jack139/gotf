1df344f1b984c1a42c76da6d1654e916215e28dd
gt <jack139@gmail.com>
2020-11-20 09:51:55 +0800

    fix

--- bert.go
+++ bert.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/buckhx/gobert/tokenize"
 	"github.com/buckhx/gobert/tokenize/vocab"
@@ -9,16 +10,18 @@ import (
 	"github.com/aclements/go-gg/generic/slice"
 )
 
+
+var corpus = `深度学习的基础是机器学习中的分散表示（distributed representation）。分散表示假定观测值是由不同因
+子相互作用生成。在此基础上，深度学习进一步假定这一相互作用的过程可分为多个层次，代表对观测值的多层抽象。不同
+的层数和层的规模可用于不同程度的抽象。深度学习运用了这分层次抽象的思想，更高层次的概念从低层次的概念学习得到。
+这一分层结构常常使用贪心算法逐层构建而成，并从中选取有助于机器学习的更有效的特征。不少深度学习算法都以无监督
+学习的形式出现，因而这些算法能被应用于其他算法无法企及的无标签数据，这一类数据比有标签数据更丰富，也更容易获
+得。这一点也为深度学习赢得了重要的优势。`
+
+//var question = "深度学习的优点是什么？"
+var question = "你是谁？"
+
 func main() {
-	corpus := "深度学习的基础是机器学习中的分散表示（distributed representation）。分散表示假定观测值是由不同因" +
-		"子相互作用生成。在此基础上，深度学习进一步假定这一相互作用的过程可分为多个层次，代表对观测值的多层抽象。不同" +
-		"的层数和层的规模可用于不同程度的抽象。深度学习运用了这分层次抽象的思想，更高层次的概念从低层次的概念学习得到。" +
-		"这一分层结构常常使用贪心算法逐层构建而成，并从中选取有助于机器学习的更有效的特征。不少深度学习算法都以无监督" +
-		"学习的形式出现，因而这些算法能被应用于其他算法无法企及的无标签数据，这一类数据比有标签数据更丰富，也更容易获" +
-		"得。这一点也为深度学习赢得了重要的优势。"
-
-	question := "深度学习的优点是什么？"
-
 	modelPath := "../../../nlp/albert_QA/outputs/saved-model"
 	vocabPath := "../../../nlp/nlp_model/albert_zh_base/vocab_chinese.txt"
 	voc, err := vocab.FromFile(vocabPath)
@@ -54,10 +57,10 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(f.Tokens)
-	fmt.Println(f.TokenIDs)
-	fmt.Println(f.Mask)
-	fmt.Println(f.TypeIDs)
+	//fmt.Println(f.Tokens)
+	//fmt.Println(f.TokenIDs)
+	//fmt.Println(f.Mask)
+	//fmt.Println(f.TypeIDs)
 
 	res, err := m.Session.Run(
 		map[tf.Output]*tf.Tensor{
@@ -74,17 +77,19 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("DataType", res[0].DataType())
-	fmt.Println("Shape", res[0].Shape())
+	//fmt.Println("DataType", res[0].DataType())
+	//fmt.Println("Shape", res[0].Shape())
 	//fmt.Println("Value", res[0].Value().([][]float32))
-
-	fmt.Println("DataType", res[1].DataType())
-	fmt.Println("Shape", res[1].Shape())
-	//fmt.Println("Value", res[1].Value().([][]float32))
 
 	st := slice.ArgMax(res[0].Value().([][]float32)[0])
 	ed := slice.ArgMax(res[1].Value().([][]float32)[0])
-	fmt.Println(st, ed)
-	fmt.Println(input_tokens[st:ed+1])
+	fmt.Println(len(f.Tokens), st, ed)
+	ans := strings.Join(f.Tokens[st:ed+1], "")
 
+	if ans != "[CLS]" { // 找到答案
+		fmt.Println("Question: ", question)
+		fmt.Println("Answer: ", ans)
+	} else {
+		fmt.Println("未找到答案。")
+	}
 }
